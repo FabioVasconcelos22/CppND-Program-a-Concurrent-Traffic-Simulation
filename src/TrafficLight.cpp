ab2dbdcecedb544e1323d4af4f9f406f797e2232
Fábio <fabio.vasconcelos@fabamaq.com>
2023-03-08 23:30:49 +0000

    Task FP.2

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -40,10 +40,13 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
-
+*/
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
+    std::thread phaseCycle (&TrafficLight::cycleThroughPhases, this);
+
+    TrafficObject::threads.emplace_back(std::move(phaseCycle));
 }
 
 // virtual function which is executed in a thread
@@ -52,7 +55,28 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+
+    constexpr int maxTime = 6;
+    constexpr int minTime = 4;
+
+    int randomCycle = rand()%(maxTime-minTime + 1) + minTime;
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+    while (true) {
+        if (_currentPhase == TrafficLightPhase::red) {
+            _currentPhase = TrafficLightPhase::green;
+        } else {
+            _currentPhase = TrafficLightPhase::red;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        auto deltaTime = std::chrono::high_resolution_clock::now() - startTime;
+
+        if (deltaTime > std::chrono::seconds(randomCycle)) {
+            break;
+        }
+    }
+    _messageQueue.send(std::move(_currentPhase));
 }
 
-*/
